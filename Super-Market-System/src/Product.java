3ed9c0b6f43be4c2560929c4deedadae38d9dec1
Kanimuthu Arumugam <kanimuthuarm@gmail.com>
2026-03-22 20:31:02 +0530

    Created Product Module for the Super Market Billing System

---
+++ Super-Market-System/src/Product.java
@@ -0,0 +1,13 @@
+class Product {
+    int id;
+    String name;
+    double price;
+    int quantity;
+
+    Product(int id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
